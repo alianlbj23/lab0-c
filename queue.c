accdb5257ea6596d83b411c4c22f617db978d540
user <alianlbj23@gmail.com>
2024-02-28 09:53:32 +0800

    Remove elements in q_free
    Remove elements from list and release resources in q_free function.

--- queue.c
+++ queue.c
@@ -21,7 +21,18 @@ struct list_head *q_new()
 }
 
 /* Free all storage used by queue */
-void q_free(struct list_head *l) {}
+void q_free(struct list_head *l)
+{
+    if (l == NULL) {
+        return;
+    }
+    element_t *c, *n;
+    list_for_each_entry_safe (c, n, l, list) {
+        list_del(&c->list);
+        q_release_element(c);
+    }
+    free(l);
+}
 
 /* Insert an element at head of queue */
 bool q_insert_head(struct list_head *head, char *s)
